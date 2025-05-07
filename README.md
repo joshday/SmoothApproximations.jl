efdc7191387dd3353361ce9d9ae66f4678d614de
Josh Day <emailjoshday@gmail.com>
2025-05-07 11:27:38 -0400

    first commit

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # SmoothApproximations
 
 [![Build Status](https://github.com/joshday/SmoothApproximations.jl/actions/workflows/CI.yml/badge.svg?branch=main)](https://github.com/joshday/SmoothApproximations.jl/actions/workflows/CI.yml?query=branch%3Amain)
+
+This package provides a collection of function closures for creating smooth approximations of discontinuous functions.
+
+# Usage
+
+```julia
+using SmoothApproximations
+using Plots
+
+f = SmoothApproximations.approx_heaviside
+
+plot(f(k=1), label = "k=1")
+plot!(f(k=5), label = "k=5")
+plot!(f(k=20), label = "k=20")
+plot!(f(k=50), label = "k=50")
+```
--- src/SmoothApproximations.jl
+++ src/SmoothApproximations.jl
@@ -1,5 +1,40 @@
 module SmoothApproximations
 
-# Write your package code here.
+abstract type SmoothTransition end
+
+#-----------------------------------------------------------------------------# approx_heaviside
+"""
+    approx_heaviside(; from=0, to=1, at=0, k=50)
+
+Approximate the heaviside step function, generalized to any step size (`from` and `to`) and
+location (`at`), using a hyperbolic tangent function with steepness parameter `k`.
+
+- Heaviside:
+| x < at | x == at | x > at |
+|:-------:|:--------:|:------:|
+| ``from`` | ``(to-from) / 2`` | ``to`` |
+
+- Approximate Heaviside:
+```math
+(to - from) * tanh(k * (x - at)) / 2 + (from + to) / 2
+```
+"""
+approx_heaviside(; from=0, to=1, at=0, k=50) = x -> (to - from) * tanh(k * (x - at)) / 2 + (from + to) / 2
+
+
+#-----------------------------------------------------------------------------# approx_min
+_approx_min(x::Number, y::Number; k=10) = (exp(-k*x) + exp(-k*y)) / (exp(x) + exp(y))
+
+"""
+    approx_min(f, val; k=10)
+
+Smooth majorization of `x -> minimum(f(x), val)`.
+"""
+approx_min(f, val; k=10) = x -> _approx_min(f(x), val; k)
+
+
+
+#-----------------------------------------------------------------------------# utils
+
 
 end
